4c996aaa1101e5bd9596b01ea5284df72ea67f04
Marco Cot <118482073+MarcoCot1982@users.noreply.github.com>
2025-05-12 17:19:41 +0100

    Actualizar Module1.vb

--- Module1.vb
+++ Module1.vb
@@ -1,10 +1,12 @@
 Function DAS() As String
     Dim objShell As Object
     Set objShell = CreateObject("WScript.Network")
+'automatically retrieves DAS from Windows Login
     DAS = Application.WorksheetFunction.Proper(objShell.UserName)
 End Function
 
 
 Function EmployeeName() As String
+'automatically retrieves username with proper caps from Windows login
     EmployeeName = Application.WorksheetFunction.Proper(Application.UserName)
 End Function
